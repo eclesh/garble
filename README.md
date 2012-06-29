199d915b745463f2db96dc9101d5f11b6d8460c7
Eric Lesh <eclesh@gmail.com>
2012-06-28 18:07:17 -0700

    garble is a daemon that tests the resiliency of network clients

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 garble
 ======
 
-garble is a daemon that does a separate evil thing over each port it listens on
+garble is a daemon that does a separate evil thing over each port it
+listens on. It can be used to test your network clients for resiliency
+when faced with failure and evil.
+
+garble was inspired by the Killer Test Harness from Michael Nygard's
+talk at Velocity 2012, ["Stablity
+Patterns"](http://velocityconf.com/velocity2012/public/schedule/detail/24841)
+(slides 44 and 45).
+
+Presently, five not-so-evil ideas are implemented:
+- Fast Close: Accept a connection then close it immediately
+- Timeout: Accept a connection, do nothing, then close after 30 seconds
+- Never Close: Accept a connection and never close it
+- Slow but Steady: Accept a connection and return one byte every second
+- Random: Accept a connection and return random binary data
+
+Ideas to be implemented:
+- Read the request at 1 byte/sec
+- Proxy the request and receive real data, but return to client at 1 byte/sec
---
+++ garble.go
@@ -0,0 +1,127 @@
+// garble is a daemon that does evil things over each port it listens on
+
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+var (
+	lAddr = flag.String("a", "localhost", "listen address")
+)
+
+type Filter struct {
+	Run func(f *Filter, conn net.Conn)
+
+	Port        int    // port this filter listens on
+	Name        string // name of the filter
+	Description string // help text for user friendliness
+}
+
+var filters = []*Filter{
+	fFastClose,
+	fTimeout,
+	fNeverClose,
+	fSlowButSteady,
+	fRandom,
+}
+
+func startFilter(f *Filter) {
+	addr := fmt.Sprintf("%s:%d", *lAddr, f.Port)
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(f.Name, err)
+	}
+
+	log.Printf("Running '%s' on port '%d'", f.Name, f.Port)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			// There's not really a need to die here
+			log.Fatal(err)
+		}
+		go f.Run(f, conn)
+	}
+
+}
+
+func main() {
+	flag.Parse()
+
+	for _, f := range filters {
+		go startFilter(f)
+	}
+
+	// Block forever while the above goroutines execute
+	select {}
+}
+
+var fFastClose = &Filter{
+	Run:         runFastClose,
+	Port:        1901,
+	Name:        "Fast Close",
+	Description: "Accept a connection then close it immediately",
+}
+
+func runFastClose(f *Filter, conn net.Conn) {
+	conn.Close()
+}
+
+var fTimeout = &Filter{
+	Run:         runTimeout,
+	Port:        1902,
+	Name:        "Timeout",
+	Description: "Accept a connection, do nothing, then close after 30 seconds",
+}
+
+func runTimeout(f *Filter, conn net.Conn) {
+	select {
+	case <-time.After(30 * time.Second):
+		conn.Close()
+	}
+}
+
+var fNeverClose = &Filter{
+	Run:         runNeverClose,
+	Port:        1903,
+	Name:        "Never Close",
+	Description: "Accept a connection and never close it",
+}
+
+func runNeverClose(f *Filter, conn net.Conn) {
+	select {}
+}
+
+var fSlowButSteady = &Filter{
+	Run:         runSlowButSteady,
+	Port:        1904,
+	Name:        "Slow but Steady",
+	Description: "Accept a connection and return one byte every second",
+}
+
+func runSlowButSteady(f *Filter, conn net.Conn) {
+	c := time.Tick(1 * time.Second)
+	for _ = range c {
+		conn.Write([]byte("g"))
+	}
+}
+
+var fRandom = &Filter{
+	Run:         runRandom,
+	Port:        1905,
+	Name:        "Random Response",
+	Description: "Accept a connection and return random binary data",
+}
+
+func runRandom(f *Filter, conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	rand.Read(buf)
+	conn.Write(buf)
+}
